7e4399c22d59f47709c10a3b6e257cc60b2e33b6
Napon Taratan <napon@napontaratan.com>
2017-01-14 14:48:40 +0700

    Simplify things a little

--- src/client/assets/javascripts/app/routes.js
+++ src/client/assets/javascripts/app/routes.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from './App';
-import FriendsView from 'features/friends/components/FriendsView';
+import Main from 'components/Main';
 import NotFoundView from 'components/NotFound';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={FriendsView} />
+    <IndexRoute component={Main} />
     <Route path="404" component={NotFoundView} />
     <Redirect from="*" to="404" />
   </Route>
---
+++ src/client/assets/javascripts/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className="container text-center">
+        <h1>This is the main page!</h1>
+        <p>Modify this component to get started</p>
+        <hr />
+      </div>
+    );
+  }
+}
